945e9b1eb518fdbc8e466c0c53cdf163d3c3a66d
AronDJacobsen <59569696+AronDJacobsen@users.noreply.github.com>
2021-06-16 21:46:58 +0200

    stat with adjusted pval

--- evaluation/statistics.py
+++ evaluation/statistics.py
@@ -1,7 +1,33 @@
 from results import *
+from sklearn.metrics import balanced_accuracy_score
+from scipy import stats
+import scikit_posthocs as sp
+import statsmodels
 
 
+def error_rate(y_true, y_pred):
+    # balanced accuracy
+    #score = balanced_accuracy_score(y_true, y_pred)
 
+    #accuracy
+    score = np.mean(y_true == y_pred)
+
+    return 1-score
+
+def error_rate2(y_true, yhatA, yhatB):
+    nn = np.zeros((2,2))
+    c1 = yhatA - y_true == 0
+    c2 = yhatB - y_true == 0
+
+    nn[0,0] = sum(c1 & c2)
+    nn[0,1] = sum(c1 & ~c2)
+    nn[1,0] = sum(~c1 & c2)
+    nn[1,1] = sum(~c1 & ~c2)
+    n = sum(nn.flat);
+
+    erA = (nn[0,0]+nn[0,1]) / n
+    erB = (nn[0,0]+nn[1,0]) / n
+    return erA, erB
 
 #imported from toolbox from data mining course
 def mcnemar(y_true, yhatA, yhatB, alpha=0.05):
@@ -52,19 +78,24 @@ if __name__ == '__main__':
     pd.set_option("display.max_rows", None, "display.max_columns", None)
 
     #Albert
-    #dir = r"C:\Users\Albert Kjøller\Documents\GitHub\EEG_epilepsia"
-    #y_true_path = r"C:\Users\Albert Kjøller\Documents\GitHub\EEG_epilepsia\results\y_true"
-    #windowsOS = True
+    dir = r"C:\Users\Albert Kjøller\Documents\GitHub\EEG_epilepsia"
+    y_true_path = r"C:\Users\Albert Kjøller\Documents\GitHub\EEG_epilepsia\results\y_true"
+    windowsOS = True
 
 
     #Aron
-    dir = r"/Users/Jacobsen/Documents/GitHub/EEG_epilepsia" + "/"
-    y_true_path = r"/Users/Jacobsen/Documents/GitHub/EEG_epilepsia/results/y_true" + "/"
-    windowsOS = False
+    #dir = r"/Users/Jacobsen/Documents/GitHub/EEG_epilepsia" + "/"
+    #y_true_path = r"/Users/Jacobsen/Documents/GitHub/EEG_epilepsia/results/y_true" + "/"
+    #windowsOS = False
 
+    #t test or mcnemar
+    t_test=True
+    #error rate 1(simple) or error rate 2(the nn scores)
+    er1 = False
 
-    #methods = ["colorNoise", "whiteNoise", "GAN", "MixUp"]
-    methods = ["MixUp"] # fast run
+    methods = ["colorNoise", "whiteNoise", "GAN", "MixUp"]
+    #methods = ["MixUp"] # fast run
+
 
     #smote(T/F), best_model, best_ratio
     #hardcoding
@@ -159,7 +190,12 @@ if __name__ == '__main__':
         for j, artifact in enumerate(artifact_names):
             print(artifact)
             # y true predictions
-            y_true = np.concatenate(y_true_dict[artifact])
+            if t_test:
+                y_true = y_true_dict[artifact]
+                with_folds = True
+            else:
+                y_true = np.concatenate(y_true_dict[artifact])
+                with_folds = False
 
 
             #finding best for this artifact and method
@@ -168,50 +204,85 @@ if __name__ == '__main__':
             best_control_model = best_control[artifact]
 
             #getting predictions
-
             if not smote: # if augmentation was best model
                 # getting predictions
-                y = obj_aug.getPredictions(models=[best_model], aug_ratios=[best_ratio], smote_ratios = [1],  withFolds=False)
+                y = obj_aug.getPredictions(models=[best_model], aug_ratios=[best_ratio], smote_ratios = [1],  withFolds=with_folds)
                 y = obj_aug.compressDict(y, smote_ratio=1, aug_ratio=best_ratio)
                 # best models predictions
                 B = y[best_model][artifact]
 
-                y_c = obj_aug.getPredictions(models=[best_control_model], aug_ratios=[0], smote_ratios = [1], withFolds=False)
+                y_c = obj_aug.getPredictions(models=[best_control_model], aug_ratios=[0], smote_ratios = [1], withFolds=with_folds)
                 y_c = obj_aug.compressDict(y_c, smote_ratio=1, aug_ratio=0)
                 A = y_c[best_control_model][artifact]
 
             else: # if improvement was best model
                 # getting predictions
-                y = obj_improv.getPredictions(models=[best_model], aug_ratios=[best_ratio], smote_ratios = [1],  withFolds=False)
+                y = obj_improv.getPredictions(models=[best_model], aug_ratios=[best_ratio], smote_ratios = [1],  withFolds=with_folds)
                 y = obj_improv.compressDict(y, smote_ratio=1, aug_ratio=best_ratio)
                 # best models predictions
                 B = y[best_model][artifact]
 
                 #getting control predictions
-                y_c = obj_improv.getPredictions(models=[best_control_model], aug_ratios=[0], smote_ratios = [1], withFolds=False)
+                y_c = obj_improv.getPredictions(models=[best_control_model], aug_ratios=[0], smote_ratios = [1], withFolds=with_folds)
                 y_c = obj_improv.compressDict(y_c, smote_ratio=1, aug_ratio=0)
                 A = y_c[best_control_model][artifact]
+            if not t_test:
+                # McNemar, from toolbox in data mining course
+                [thetahat, CI, p] = mcnemar(y_true, A, B, alpha = 0.05)
+                #investigate if A is better than B
+                #H0: have same performance
+                #thetahat is difference in accuracy, A-B
+                #CI is condfidence interval of this difference
+                #p is pvalue, low means reject H0 of same performance
+            else:
+                if er1:
+                    ERA = []
+                    ERB = []
+                    for i in folds:
+                        ERA.append(error_rate(y_true[i], A[i]))
+                        ERB.append(error_rate(y_true[i], B[i]))
 
-            # McNemar, from toolbox in data mining course
-            [thetahat, CI, p] = mcnemar(y_true, A, B, alpha = 0.05)
-            #investigate if A is better than B
-            #H0: have same performance
-            #thetahat is difference in accuracy, A-B
-            #CI is condfidence interval of this difference
-            #p is pvalue, low means reject H0 of same performance
+                    t, p = stats.ttest_rel(ERA, ERB)
+                else:
+                    ERA = []
+                    ERB = []
+                    for i in folds:
+                        erA, erB = error_rate2(y_true[i], A[i], B[i])
+                        ERA.append(erA)
+                        ERB.append(erB)
+                    t, p = stats.ttest_ind(ERA, ERB)
+
+
+
+
+
+        # add this p value
             artifact_list.append(float(f"{p:.3e}")) # modifying format
-
         df.loc[method] = artifact_list
         stop=0
 
     #getting numbers
     df = df.astype(float)
 
-    print('Dataframe:')
+    print('p-values:')
     print(df)
 
     print('Latex format:')
     df_latex = df.to_latex()
+    print(df_latex)
+
+    print('\n\n')
+    print('Adjusted p-values')
+    #performing adjusted p values
+    arr = statsmodels.stats.multitest.multipletests(np.concatenate(df.values), alpha=0.05, method = 'fdr_bh', returnsorted=False)[1]
+    mat = np.reshape(arr, (len(methods), len(artifact_names)))
+    df_adj = pd.DataFrame(columns=artifact_names, index=methods)
+    for i, method in enumerate(methods):
+        df_adj.loc[method] = mat[i]
+    print(df_adj)
+
+    print('Latex format:')
+    df_latex = df_adj.to_latex()
     print(df_latex)
 
 
